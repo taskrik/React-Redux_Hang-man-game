820a86007b6e053f7e7ba5f443a75f88c38c79af
tasos <tasos.krikonis@gmail.com>
2018-06-16 09:55:11 +0200

    fixed setup

--- src/App.js
+++ src/App.js
@@ -1,22 +1,20 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import {Provider} from 'react-redux'
-import store from './store'
+
 
 class App extends Component {
   render() {
     return (
 
-      <Provider store={store}>
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to HangMan</h1>
+          <h1 className="App-title">Welcome to React</h1>
         </header>
 
       </div>
-    </Provider>
+
     );
   }
 }
